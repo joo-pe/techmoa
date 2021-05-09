e8c9ca32e3c0f6a3bdc2a46329f17647e8bee9b4
joo-pe <joohyung1002@naver.com>
2021-05-09 21:49:33 +0900

    :sparkles: git init

---
+++ src/main/java/kr/co/techmoa/api/TechmoaApplication.java
@@ -0,0 +1,11 @@
+package kr.co.techmoa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmoaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechmoaApplication.class , args);
+    }
+}
